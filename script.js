856b326b15c6281d273a7a4930d6df86b2e0d5ee
Andrew Riess <riessandrew@yahoo.com>
2025-07-15 00:18:44 -0700

    padding fixes for mobile

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Version info - updated during build/commit
-const VERSION = '4382e81'; // Will be replaced with git hash
+const VERSION = '8d309ad'; // Will be replaced with git hash
 
 // Card counting trainer
 class BlackjackGame {
