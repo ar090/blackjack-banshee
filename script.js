53988b659bf1d66aa9d4dae1c198c0252bb6a8ef
Andrew Riess <riessandrew@yahoo.com>
2025-07-15 00:22:39 -0700

    Deploy: Update version to b779800

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Version info - updated during build/commit
-const VERSION = '2974710'; // Will be replaced with git hash
+const VERSION = 'b779800'; // Will be replaced with git hash
 
 // Card counting trainer
 class BlackjackGame {
